de367f100a923a5ef5c365cb85e744a17e167683
rylan <sedlackerylan@gmail.com>
2025-05-26 15:13:26 -0400

    Neuron shell and training shell

---
+++ sed_nn.py
@@ -0,0 +1,36 @@
+from sed_engine import Value
+import random
+
+class Neuron:
+    def __init__(self, num_inputs):
+        self.weights = []
+        for i in range(num_inputs):
+            weight = Value(random.uniform(-1,1))
+            self.weights.append(weight)
+        
+        self.bias = Value(0.0)
+
+    def __call__(self, inputs):
+        total = self.bias
+
+        for i in range(len(self.weights)):
+            product = self.weights[i] * inputs[i]
+            total += product
+
+        output = total.tanh()
+        return output
+
+    def parameters(self):
+        params = []
+
+        for a in self.weights:
+            params.append(a)
+        params.append(self.bias)
+        return params
+        
+
+
+
+        
+  
+
---
+++ sed_train.py
@@ -0,0 +1,3 @@
+from sed_nn import Neuron
+from sed_engine import Value
+
